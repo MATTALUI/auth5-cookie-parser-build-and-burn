18ea73cc47a74a8404793b0a3376f35806f44d11
Matthew Hummer <biggreyson@gmail.com>
2017-05-30 13:49:13 -0600

    nothing but net

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 
+app.use(cookieParser());
 app.get('/set', (req,res,next) => {
-  
+  res.cookie('foo', 'bar');
+  res.send();
 });
 
 app.get('/read', (req,res,next) => {
-
+  res.send(req.cookies);
 });
 
 app.get('/clear', (req,res,next) => {
-
+  res.clearCookie('deleteMe');
+ res.send();
 });
 
 app.listen('3000', () => {
